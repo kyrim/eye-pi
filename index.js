82ab70cadbd2dd1a7a4b062a7c1e16c852842e61
kyrimsteele <kyrimcsteele@gmail.com>
2017-10-18 00:35:40 +1100

    Make streaming of camera to browser work

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 "use strict";
 const path = require("path");
 const Koa = require("koa");
+const websockify = require("koa-websocket");
+
+const Serve = require("koa-static");
 const Router = require("koa-router");
+const through = require("through");
+var Stream = require("stream");
 
 const worker = require("streaming-worker");
 const addon_path = path.join(__dirname, "./build/Release/eye-pi");
 const eye_pi = worker(addon_path);
 
-eye_pi.from.on("integer", function(value) {
-  console.log(value);
-});
+// eye_pi.from.on("eyepi", function(value) {
+//   console.log(value);
+// });
 
 // const through = require("through");
 // var output = eye_pi.from.stream();
@@ -24,18 +29,87 @@ eye_pi.from.on("integer", function(value) {
 //   )
 //   .pipe(process.stdout);
 
-const koa = new Koa();
-const app = new Router();
+const app = new Koa();
+let sock = websockify(app);
+
+const router = new Router();
+
+app.use(Serve(__dirname + "/client"));
 
 const PORT = 3000;
 
 // response
-app.get("/", async ctx => {
-  ctx.body = "Welcome to eye-pi";
+// app.get("/", async ctx => {
+//   ctx.body = "Welcome to eye-pi";
+// });
+
+router.get("/stream", async ctx => {
+  eye_pi.from.on("eyepi", function(value) {
+    var json = JSON.parse(value);
+    var frame = json.frame;
+    ctx.websocket.send(frame);
+  });
 });
 
-koa.use(app.routes());
+// app.ws.use(
+//   route.all("/stream", function(ctx) {
+//     // `ctx` is the regular koa context created from the `ws` onConnection `socket.upgradeReq` object.
+//     // the websocket is added to the context on `ctx.websocket`.
+//     ctx.websocket.send("Hello World");
+//     ctx.websocket.on("message", function(message) {
+//       // do something with the message from client
+//       console.log(message);
+//     });
+//   })
+// );
+
+// router.get("/stream", async ctx => {
+//   var stream = (this.body = new Stream.PassThrough());
+
+//   eye_pi.from
+//     .stream()
+//     .pipe(
+//       through(function(data) {
+//         var json = JSON.parse(data[1]);
+//         var frame = json.frame;
+//         var returnString = `Content-Type: image/jpeg\n Content-Length: ${frame.length}\n\n${frame}\n--${boundary}\n`;
+//         this.queue(returnString);
+//       })
+//     )
+//     .pipe(stream);
+
+//   ctx.body = stream;
+// });
+
+// router.use(function*(next) {
+//   if (this.path !== "/stream") return yield next;
+
+//   this.set("Content-Type", "text/event-stream");
+//   this.set("Cache-Control", "max-age=0, no-cache");
+//   this.set("Connection", "keep-alive");
+
+//   var stream = (this.body = new Stream.PassThrough());
+
+//   snapshots.pipe(stream);
+
+//   this.req.socket.once("close", function() {
+//     snapshots.unpipe(stream);
+//   });
+// });
+
+// router.use(function*(next) {
+//   if (this.path !== "/") return yield next;
+
+//   this.body = template.replace(
+//     "{{src}}",
+//     lastSnapshot ? "data:image/jpeg;base64," + lastSnapshot : ""
+//   );
+// });
+
+app.ws.use(router.routes()).use(router.allowedMethods());
+
+//app.use(router.routes());
 
 console.log(`Starting on port: ${PORT}`);
 
-koa.listen(PORT);
+app.listen(PORT);
--- native/main.cpp
+++ native/main.cpp
@@ -2,37 +2,12 @@
 #include "MotionDetection/MotionDetector.hpp"
 #include <iostream>
 #include <opencv2/opencv.hpp>
-#include <nan.h>
+#include "streaming-worker.h"
+#include "Utility/json.hpp"
+#include "Utility/base64.hpp"
 
 using namespace std;
-
-bool HandleRecording(bool newVideo, const std::string &videoFileName,
-					 int dWidth, int dHeight, cv::VideoWriter &videoWriter,
-					 const cv::Size &frameSize, cv::Mat &currentFrame,
-					 double fps)
-{
-	if (newVideo)
-	{
-		std::cout << "File has been opened for writing: " << videoFileName
-				  << std::endl;
-		std::cout << "Frame Size = " << dWidth << "x" << dHeight << std::endl;
-
-		videoWriter = cv::VideoWriter(videoFileName, CV_FOURCC('D', 'I', 'V', '3'),
-									  fps, frameSize, true);
-	}
-
-	if (!videoWriter.isOpened())
-	{
-		std::cout << "ERROR: Failed to initialize video writing" << std::endl;
-		getchar();
-
-		return false;
-	}
-
-	videoWriter.write(currentFrame);
-
-	return true;
-}
+using json = nlohmann::json;
 
 void handleFrame(cv::UMat &frame, std::unique_ptr<cv::Rect> rect_ptr)
 {
@@ -44,66 +19,66 @@ void handleFrame(cv::UMat &frame, std::unique_ptr<cv::Rect> rect_ptr)
 					  cv::Point(rect.x + rect.width, rect.y + rect.height), cv::Scalar(0, 255, 0),
 					  2);
 	}
-
-	cv::imshow("Stream", frame);
 }
 
-int main(int argc, char **argv)
+class EyePi : public StreamingWorker
 {
-	CameraStreamer cam(0);
-	MotionDetector motion_detector(cam);
-	auto textPoint = cv::Point(30, 30);
-	auto textScalar = cv::Scalar(0, 0, 255);
-
-	while (true)
+  public:
+	EyePi(Callback *data, Callback *complete, Callback *error_callback, v8::Local<v8::Object> &options)
+		: StreamingWorker(data, complete, error_callback)
 	{
-		auto start = cv::getTickCount();
-		auto m_frame = motion_detector.getNextFrame();
-
-		if (m_frame == nullptr)
-			continue;
-
-		auto frame = m_frame->getFrame();
-		auto rect = m_frame->getRectangle();
-		auto fps = cv::getTickFrequency() / (cv::getTickCount() - start);
-
-		cv::putText(frame, std::to_string(fps), textPoint, cv::FONT_HERSHEY_SIMPLEX,
-					0.8, textScalar);
-
-		handleFrame(frame, move(rect));
-
-		if (cv::waitKey(1) == 27)
-			return 0;
 	}
+
+	void Execute(const AsyncProgressWorker::ExecutionProgress &progress)
+	{
+		CameraStreamer cam(0);
+		MotionDetector motion_detector(cam);
+		auto textPoint = cv::Point(30, 30);
+		auto textScalar = cv::Scalar(0, 0, 255);
+
+		json sample;
+
+		while (!closed())
+		{
+			auto start = cv::getTickCount();
+			auto m_frame = motion_detector.getNextFrame();
+
+			if (m_frame == nullptr)
+				continue;
+
+			auto frame = m_frame->getFrame();
+			auto rect = m_frame->getRectangle();
+			auto fps = cv::getTickFrequency() / (cv::getTickCount() - start);
+
+			cv::putText(frame, std::to_string(fps), textPoint, cv::FONT_HERSHEY_SIMPLEX,
+						0.8, textScalar);
+
+			handleFrame(frame, move(rect));
+
+			json data;
+
+			cv::Mat mat = frame.getMat(cv::ACCESS_READ);
+			int len = mat.total() * mat.elemSize();
+
+			std::vector<uchar> buf;
+			imencode(".jpg", frame, buf);
+			uchar *enc_msg = new uchar[buf.size()];
+
+			for (int i = 0; i < buf.size(); i++)
+				enc_msg[i] = buf[i];
+
+			data["sensor"] = "sensor";
+			data["frame"] = base64_encode(enc_msg, buf.size());
+
+			Message tosend("eyepi", data.dump());
+			writeToNode(progress, tosend);
+		}
+	}
+};
+
+StreamingWorker *create_worker(Callback *data, Callback *complete, Callback *error_callback, v8::Local<v8::Object> &options)
+{
+	return new EyePi(data, complete, error_callback, options);
 }
 
-// if (motionDetected)
-// {
-// 	cout << "Motion detected, recording" << endl;
-// 	motionCounter += 1;
-// 	if (motionCounter >= 2)
-// 	{
-
-// 		if (!recording)
-// 		{
-// 			videoFileName = GetTime() + ".avi";
-// 		}
-
-// 		while (!recordingBuffer.empty())
-// 		{
-// 			HandleRecording(!recording,
-// 							videoFileName,
-// 							dWidth,
-// 							dHeight, videoWriter,
-// 							frameSize,
-// 							recordingBuffer.front(),
-// 							fps);
-
-// 			recordingBuffer.pop();
-// 			recording = true;
-// 		}
-// 		motionCounter = 1;
-// 	}
-// }
-
-// capture.release();
+NODE_MODULE(eye_pi, StreamWorkerWrapper::Init)
